386df8746ff722dfd50d1f5e49f539ab9949a1f9
Ethan Done <yichen_1226@163.com>
2025-11-23 13:54:33 +0800

    Revert "refactor: socket => trait"
    This reverts commit e3a77154b5722a6009500be10589c1ab3575058a.

--- libsponge/src/util/tun.rs
+++ libsponge/src/util/tun.rs
@@ -1,4 +1,4 @@
-use crate::{FDWrapper, FileDescriptor, system_call};
+use crate::{FileDescriptor, system_call};
 
 use anyhow::Result;
 use libc::{
@@ -9,53 +9,77 @@ use std::{ffi::CString, mem::zeroed, ptr::copy_nonoverlapping};
 
 const CLONEDEV: &str = "/dev/net/tun";
 
-trait DeviceType: FileDescriptor {
+pub trait DeviceType: sealed::Sealed {
     const IS_TUN: bool;
     const IFF_FLAG: c_int;
-
-    fn try_new_tuntap(dev_name: &str) -> Result<FDWrapper> {
-        let fd = system_call("open", || unsafe {
-            open(CLONEDEV as *const str as *const i8, O_RDWR)
-        })?;
-
-        let fdw = FDWrapper::try_from(fd)?;
-        let mut tun_req: ifreq = unsafe { zeroed() };
-        let c_name = CString::new(dev_name)?;
-        let name_bytes = c_name.as_bytes_with_nul();
-        let copy_len = IFNAMSIZ.min(name_bytes.len());
-
-        unsafe {
-            copy_nonoverlapping(
-                name_bytes.as_ptr() as *const i8,
-                tun_req.ifr_name.as_mut_ptr(),
-                copy_len,
-            );
-        }
-
-        tun_req.ifr_ifru.ifru_flags = Self::IFF_FLAG as _;
-
-        system_call("ioctl", || unsafe {
-            ioctl(fdw.fd, TUNSETIFF, &tun_req as *const ifreq as *const c_void)
-        })?;
-
-        Ok(fdw)
-    }
 }
 
-pub trait Tun: DeviceType {
+mod sealed {
+    pub trait Sealed {}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Tun;
+impl sealed::Sealed for Tun {}
+impl DeviceType for Tun {
     const IS_TUN: bool = true;
     const IFF_FLAG: c_int = IFF_TUN | IFF_NO_PI;
+}
 
-    fn try_new(dev_name: &str) -> Result<FDWrapper> {
-        Self::try_new_tuntap(dev_name)
+#[derive(Debug, Clone, Copy)]
+pub struct Tap;
+impl sealed::Sealed for Tap {}
+impl DeviceType for Tap {
+    const IS_TUN: bool = false;
+    const IFF_FLAG: c_int = IFF_TAP | IFF_NO_PI;
+}
+
+fn try_new_tuntap<A: Default + Clone, T: DeviceType, P>(
+    dev_name: &str,
+) -> Result<FileDescriptor<A, T, P>> {
+    let fd = system_call("open", || unsafe {
+        open(CLONEDEV as *const str as *const i8, O_RDWR)
+    })?;
+
+    let this = FileDescriptor::from(fd);
+    let mut tun_req: ifreq = unsafe { zeroed() };
+    let c_name = CString::new(dev_name)?;
+    let name_bytes = c_name.as_bytes_with_nul();
+    let copy_len = IFNAMSIZ.min(name_bytes.len());
+
+    unsafe {
+        copy_nonoverlapping(
+            name_bytes.as_ptr() as *const i8,
+            tun_req.ifr_name.as_mut_ptr(),
+            copy_len,
+        );
+    }
+
+    tun_req.ifr_ifru.ifru_flags = T::IFF_FLAG as _;
+
+    system_call("ioctl", || unsafe {
+        ioctl(
+            this.fd(),
+            TUNSETIFF,
+            &tun_req as *const ifreq as *const c_void,
+        )
+    })?;
+
+    Ok(this)
+}
+
+pub type TunFD<A, P> = FileDescriptor<A, Tun, P>;
+
+impl<A: Default + Clone, P> TunFD<A, P> {
+    pub fn try_new(dev_name: &str) -> Result<Self> {
+        try_new_tuntap(dev_name)
     }
 }
 
-pub trait Tap: DeviceType {
-    const IS_TUN: bool = false;
-    const IFF_FLAG: c_int = IFF_TAP | IFF_NO_PI;
+pub type TapFD<A, P> = FileDescriptor<A, Tap, P>;
 
-    fn try_new(dev_name: &str) -> Result<FDWrapper> {
-        Self::try_new_tuntap(dev_name)
+impl<A: Default + Clone, P> TapFD<A, P> {
+    pub fn try_new(dev_name: &str) -> Result<Self> {
+        try_new_tuntap(dev_name)
     }
 }
